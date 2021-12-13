235ee3296e876e53b14960eb7d726988980261d6
Akshay <akshay.pansari144@gmail.com>
2021-12-14 03:01:21 +0530

    google test

---
+++ src/main_file.cpp
@@ -0,0 +1,19 @@
+// divide 2 numbers and then test the code
+// if number is 0, it should return some error
+// else it should divide the number and make it floor
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+
+    int c = a/b;
+    cout<<c<<endl;
+
+    return 0;
+
+}
